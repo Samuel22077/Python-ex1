16c5dbe4a58e40103b059fa4ae7b91109693f82f
Samuel__ <samuelmtonon@gmail.com>
2026-02-24 17:38:15 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,18 @@
+#altura=float (input("Digite sua altura: "))
+#Peso_Ideal= (72.7* altura) -58
+#print("Seu peso Ideal é: %.2f" %Peso_Ideal)
+
+#KMS=float (input("Digite a quantidade de quilometros: "))
+#Dias=float(input("Digite a quantidade de dias: "))
+#Total= (KMS*0.15)+(Dias*60)
+#print("Total a pagar: %.2f" %Total)
+
+#Dias=float(input("Digite a quantidade de dias: "))
+#Horas=float(input("Digite a quantidade de horas: "))
+#Minutos=float(input("Digite a quantidade de minutos: "))
+#Segundos=float(input("Digite a quantidade de segundos: "))
+#Tempo_total= (Dias*86400)+(Horas*3600)+(Minutos*60)+Segundos
+#print("Tempo total foi de: %.2f" %Tempo_total)
+
+Hora_Trabalhada=float(input("Digite o valor dahora de trabalho: "))
+Horas_do_mês=float(input("Digite o numero de horas trabalhadas no mês: "))
